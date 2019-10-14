184ab70cac174fc356dad74397b07e5a1d84e580
unknown <1456421562@qq.com>
2019-10-14 23:42:51 +0800

    _app.js的深入理解

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,9 @@ import Layout from '../components/Layout'
 
 class MyApp extends App {
   // 全局性的数据获取 _app 修改默认的app全局配置  每个组件切换时都会作用到
+  // nextjs内部封装的默认方法
   static async getInitialProps({Component,ctx}){
+    console.log(Component)
     let pageProps
     if(Component.getInitialProps){
       pageProps = await Component.getInitialProps(ctx)
@@ -20,6 +22,7 @@ class MyApp extends App {
    render() {
      const { Component, pageProps} = this.props
      console.log(pageProps)
+     console.log(Component)
      return (
        <div>
          <Layout>
